db37b5849df7d024c1fd1264b9d040e06e474a27
Tekyto <Tekyto2014@gmail.com>
2022-11-16 16:06:52 +0200

    switch

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function evenOrOdd(n) {
-	// write your code here
+	switch (true) {
+		case n < 0: return 'NEGATIVE';
+		case n === 0: return 'ZERO';
+		case n % 2 === 1: return 'ODD';
+		case n % 2 === 0: return 'EVEN';
+	}
 }
 
 module.exports = evenOrOdd;
